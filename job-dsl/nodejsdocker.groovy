cc8c43bd27b1c59977ad7c4cf3321c124550b342
malathi-vangeepuram <mvangeepuram@gmail.com>
2021-01-27 13:45:06 -0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/mvangeepuramcker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/mvangeepuram/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('mvangeepuram@gmail.com')
         }
