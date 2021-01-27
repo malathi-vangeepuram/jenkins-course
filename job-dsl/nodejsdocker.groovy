bafd4122b6360b908139408fe8edb249cca4da7c
malathi-vangeepuram <mvangeepuram@gmail.com>
2021-01-27 11:04:59 -0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('mvangeepuram/docker-nodejs-demo')
+            repositoryName('mvangeepuram/jobdsl')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
